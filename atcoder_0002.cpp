3d7ff5b448538d8decce1ae3dea13935bbaf779f
fish-svg <mizu.yokan.taku@gmail.com>
2024-06-16 23:31:21 +0900

    second code

---
+++ atcoder_0002.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+#include <tuple>
+#include <bitset>
+#include <bits/stdc++.h>
+long long INF = 1LL << 60;
+
+int main(){
+  
+  string s, t;
+  cin >> s >> t;
+  
+  if(s == "AtCoder" and t == "Land"){
+    cout<<"Yes";
+  }
+  else{
+    cout<<"No";
+  }
+  return 0;
+}
